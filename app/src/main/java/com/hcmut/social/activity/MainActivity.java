6cd95a437417f9d9f3a367da7c4c808d7cbd0234
Long_Quoc <longnguyencse@gmail.com>
2016-10-21 15:28:35 +0700

    custom tab layout

--- app/src/main/java/com/hcmut/social/activity/MainActivity.java
+++ app/src/main/java/com/hcmut/social/activity/MainActivity.java
@@ -3,12 +3,14 @@ package com.hcmut.social.activity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.hcmut.social.R;
 import com.hcmut.social.adapter.PagerAdapter;
 
 
 public class MainActivity extends BaseActivity {
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +23,22 @@ public class MainActivity extends BaseActivity {
                 MainActivity.this));
 
         // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+        /*Set Tab view custom*/
+        customViewTabLayout(R.layout.view_home_tab, 0);
+        customViewTabLayout(R.layout.view_search_tab, 1);
+        customViewTabLayout(R.layout.view_favorites_tab, 2);
+        customViewTabLayout(R.layout.view_mypage_tab, 3);
+
+    }
+
+    private void customViewTabLayout(int resource, int index) {
+        View v = View.inflate(this.getBaseContext(),resource, null);
+        if (v == null) {
+            throw new RuntimeException("View is null");
+        }
+        tabLayout.getTabAt(index).setCustomView(v);
     }
 
     @Override
